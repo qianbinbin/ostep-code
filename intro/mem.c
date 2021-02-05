5128f2487b49be06a1f7de83f119d02ee173df82
Binbin Qian <qianbinbin@hotmail.com>
2021-02-05 22:06:36 +0800

    Update mem.c

--- intro/mem.c
+++ intro/mem.c
@@ -4,19 +4,16 @@
 #include "common.h"
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) { 
-	fprintf(stderr, "usage: mem <value>\n"); 
-	exit(1); 
-    } 
-    int *p; 
-    p = malloc(sizeof(int));
+    int *p = malloc(sizeof(int));
     assert(p != NULL);
-    printf("(%d) addr pointed to by p: %p\n", (int) getpid(), p);
-    *p = atoi(argv[1]); // assign value to addr stored in p
-    while (1) {
-	Spin(1);
-	*p = *p + 1;
-	printf("(%d) value of p: %d\n", getpid(), *p);
+    printf("(%d) address pointed to by p: %p\n",
+           getpid(), p);
+    *p = 0;
+    while (1)
+    {
+        Spin(1);
+        *p = *p + 1;
+        printf("(%d) p: %d\n", getpid(), *p);
     }
     return 0;
 }
